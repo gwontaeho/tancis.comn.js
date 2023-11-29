15afd2d29c3b09f82acd91bbf4d29b8e483b9488
claud-bae <claud6998@gmail.com>
2023-11-29 14:29:18 +0900

    @

--- src/sample/SampleRoutes.js
+++ src/sample/SampleRoutes.js
@@ -3,88 +3,103 @@ export const SampleRoutes = [
         name: "Sample",
         base: "/sample",
         children: [
-          {
-            name: "Controls",
-            base: "/controls",
-            children: [
-              {
-                name: "Text",
-                to: "/text",
-              },
-              {
-                name: "Number",
-                to: "/number",
-              },
-              {
-                name: "Password",
-                to: "/password",
-              },
-              {
-                name: "Select",
-                to: "/select",
-              },
-              {
-                name: "Radio",
-                to: "/radio",
-              },
-              {
-                name: "Checkbox",
-                to: "/checkbox",
-              },
-              {
-                name: "Textarea",
-                to: "/textarea",
-              },
-              {
-                name: "Date",
-                to: "/date",
-              },
-              {
-                name: "Time",
-                to: "/time",
-              },
-              {
-                name: "Datetime",
-                to: "/datetime",
-              },
-              {
-                name: "Between",
-                to: "/between",
-              },
-              {
-                name: "File",
-                to: "/file",
-              },
-            ],
-          },
-          {
-            name: "Group",
-            to: "/group",
-          },
-          {
-            name: "Table",
-            to: "/table",
-          },
-          {
-            name: "Form",
-            to: "/form",
-          },
-          {
-            name: "Grid",
-            to: "/grid",
-          },
-          {
-            name: "Tab",
-            to: "/tab",
-          },
-          {
-            name: "Tree",
-            to: "/tree",
-          },
-          {
-            name: "Wijmo",
-            to: "/wijmo",
-          },
+            {
+                name: "Sample Component",
+                base: "/components",
+                children: [
+                    {
+                        name: "Controls",
+                        base: "/controls",
+                        children: [
+                            {
+                                name: "Text",
+                                to: "/text",
+                            },
+                            {
+                                name: "Number",
+                                to: "/number",
+                            },
+                            {
+                                name: "Password",
+                                to: "/password",
+                            },
+                            {
+                                name: "Select",
+                                to: "/select",
+                            },
+                            {
+                                name: "Radio",
+                                to: "/radio",
+                            },
+                            {
+                                name: "Checkbox",
+                                to: "/checkbox",
+                            },
+                            {
+                                name: "Textarea",
+                                to: "/textarea",
+                            },
+                            {
+                                name: "Date",
+                                to: "/date",
+                            },
+                            {
+                                name: "Time",
+                                to: "/time",
+                            },
+                            {
+                                name: "Datetime",
+                                to: "/datetime",
+                            },
+                            {
+                                name: "Between",
+                                to: "/between",
+                            },
+                            {
+                                name: "File",
+                                to: "/file",
+                            },
+                        ],
+                    },
+                    {
+                        name: "Group",
+                        to: "/group",
+                    },
+                    {
+                        name: "Table",
+                        to: "/table",
+                    },
+                    {
+                        name: "Form",
+                        to: "/form",
+                    },
+                    {
+                        name: "Grid",
+                        to: "/grid",
+                    },
+                    {
+                        name: "Tab",
+                        to: "/tab",
+                    },
+                    {
+                        name: "Tree",
+                        to: "/tree",
+                    },
+                    {
+                        name: "Wijmo",
+                        to: "/wijmo",
+                    },
+                ],
+            },
+            {
+                name: "Sample Page",
+                base: "/pages",
+                children: [
+                    {
+                        name: "List",
+                    },
+                ],
+            },
         ],
     },
 ];
